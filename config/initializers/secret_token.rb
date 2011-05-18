623fb47ac906ffe3f85a99293d406adbcbc122a4
Mauro Otonelli <mauro.otonelli@gmail.com>
2011-05-18 14:15:03 -0300

    testapp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = '34f3569b3b7c491e79d10f0ba0a77ee62232ab71fe5844661168b0ffe849be9a67636bc8d0c249fa06a78799ee664993e1d84765891cb559df843a2a22bbd268'
---
+++ db/migrate/20110518161200_create_movies.rb
@@ -0,0 +1,16 @@
+class CreateMovies < ActiveRecord::Migration
+  def self.up
+    create_table :movies do |t|
+      t.string :title
+      t.string :cuevana
+      t.float :rating
+      t.integer :year
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :movies
+  end
+end
